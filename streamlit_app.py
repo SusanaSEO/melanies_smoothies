6108ffe21fbe1de894eb22c28faef8c7c909e7df
SusanaSEO <56410837+SusanaSEO@users.noreply.github.com>
2025-10-07 12:33:38 +0200

    Update streamlit_app.py
    add additional code to get nutritional info

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,4 +51,11 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered!', icon="✅")
 
-    
+
+
+#New seciton to display my.SMOOTHIEFROOT.com nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.SMOOTHIEFROOT.com/api/fruit/watermelon")
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_with=True)
+
