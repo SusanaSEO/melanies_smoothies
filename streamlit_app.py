9548ee75e8061d4f35adfe1f7c1c736553f86b02
SusanaSEO <56410837+SusanaSEO@users.noreply.github.com>
2025-10-07 12:43:21 +0200

    Update streamlit_app.py
    add variable to get the nutritional info for it

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,10 +33,10 @@ if ingredients_list:
 
     ingredients_string = ''
 
-    for fruit_choosen in ingredients_list:
-        ingredients_string += fruit_choosen + ' '
-
-        smoothiefroot_response = requests.get("https://my.SMOOTHIEFROOT.com/api/fruit/watermelon")
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.SMOOTHIEFROOT.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)  
